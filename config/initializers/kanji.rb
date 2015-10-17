77254e276b92ce8d4d547c0910a21912f78ed169
jtsaito <jahn.saito@gmail.com>
2015-10-17 19:17:19 +0200

    Load fixtures for testing

--- config/initializers/kanji.rb
+++ config/initializers/kanji.rb
@@ -1,1 +1,7 @@
-Kernel.const_set("KanjiDataPath", Rails.root.join('config', 'kanji.yml'))
+kanji_config_path = if Rails.env.test?
+  Rails.root.join('spec', 'fixtures', 'kanji.yml')
+else
+  Rails.root.join('config', 'kanji.yml')
+end
+
+Kernel.const_set("KanjiDataPath", kanji_config_path)
