3bb0485517cc272fc55586ba4444f339f1430202
JT Saito <jahns@odaiba.coova.org>
2015-11-08 13:14:42 +0100

    Use aws cli instead of wget

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -1,3 +1,3 @@
 # Asset pre-compilation for OpsWorks cook book
 run "cd #{release_path} && RAILS_ENV=production bundle exec rake assets:precompile"
-run "wget $KANJI_MEISTER_URL -O config/kanji.yml"
+run "aws s3 cp s3://$KANJI_MEISTER_CONFIG_FILE config/kanji.yml --region KANJI_MEISTER_BUCKET_REGION"
