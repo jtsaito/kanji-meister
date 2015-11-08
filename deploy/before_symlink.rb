18c7ba62228baff016f5a987830db74326a32c34
JT Saito <jahns@odaiba.coova.org>
2015-11-08 13:16:31 +0100

    Add missing dollar sign

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -1,3 +1,3 @@
 # Asset pre-compilation for OpsWorks cook book
 run "cd #{release_path} && RAILS_ENV=production bundle exec rake assets:precompile"
-run "aws s3 cp s3://$KANJI_MEISTER_CONFIG_FILE config/kanji.yml --region KANJI_MEISTER_BUCKET_REGION"
+run "aws s3 cp s3://$KANJI_MEISTER_CONFIG_FILE config/kanji.yml --region $KANJI_MEISTER_BUCKET_REGION"
