e4fd94bd38c28a945ba81fa96987f7fd6fe3162e
jtsaito <jahn.saito@gmail.com>
2016-02-08 21:25:09 +0100

    Spa generated without boostrap 3.3.6, jquery ui 1.11.4

--- public/javascripts/spa.js
+++ public/javascripts/spa.js
@@ -486,6 +486,13 @@
 //
 
 //
+//# require application/lib/jquery-1.12.0.min
+//# require application/lib/underscore-min
+//# require application/lib/bootstrap
+//# require application/lib/app
+
+//# require application/lib/backbone
+
 
 
 
